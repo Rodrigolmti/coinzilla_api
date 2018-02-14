9ea0de477b40ab46d9ee6f257b5c8a3080d496e5
Rodrigo Lopes <rodrigolmti@gmail.com>
2018-02-14 19:05:03 -0200

    Remove asic and gpu api

--- app/crawler/whatToMine.js
+++ app/crawler/whatToMine.js
@@ -2,67 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
 
-var urlWhatToMineGpu = "https://whattomine.com";
-var urlWhatToMineAsic = "https://whattomine.com/asic";
-
 var urlCoinWarz = "https://www.coinwarz.com/cryptocurrency";
-
-var getWhatToMine = function (model, url) {
-
-    request(url, function (error, response, body) {
-        
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(body, {
-                ignoreWhitespace: true,
-                xmlMode: true
-            });
-
-            $('table tbody tr').each(function (index, row) {
-                var cell = $(row).find('td');
-
-                var tag = cell.children('div').next().children('a').text();
-                var algorithm = cell.children('div').next().next().text();
-                var difficulty = cell.next().next().children('div').children('strong').text();
-                var netHash = cell.next().next().children('div').children('div').eq(0).text();
-                var estSplit = cell.next().next().next().children('div').eq(0).text().split(" ");
-                var marketSplit = cell.next().next().next().next().next().eq(0).text().split(" ");
-
-                var estResward = estSplit[1]
-                var estResward24 = estSplit[2];
-                var marketCap = marketSplit[1];
-                var volume = marketSplit[3];
-
-                if (tag) {
-                    var metadata = {
-                        tag: tag.trim(),
-                        algorithm: algorithm.trim(),
-                        difficulty: difficulty.trim(),
-                        netHash: netHash.trim(),
-                        estResward: estResward.trim(),
-                        estResward24: estResward24.trim(),
-                        marketCap: marketCap.trim(),
-                        volume: volume.trim(),
-                        updateDate: new Date()
-                    };
-
-                    if (metadata) {
-                        model.remove({})
-                            .then(function(item) {
-                                model.create(metadata)
-                                    .then(function(item){ },
-                                    function(error) {
-                                        console.log(error);
-                                    });
-                            },
-                            function(error) {
-                                console.log(error);
-                            });
-                    }
-                }
-            });
-        }
-    });
-};
 
 var getCoinWarZ = function () {
 
@@ -132,15 +72,12 @@ var getCoinWarZ = function () {
                             console.log(error);
                         });
                 }
-
             });
         }
     });
 };
 
 var start = function () {
-    getWhatToMine(mongoose.model('CoinWTMGpu'), urlWhatToMineGpu);
-    getWhatToMine(mongoose.model('CoinWTMAsic'), urlWhatToMineAsic);
     getCoinWarZ();
 };
 
