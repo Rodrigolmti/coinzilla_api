7bb9187bc0d40b1507ed0a3054bd844a8282572b
Rodrigo Lopes <rodrigolmti@gmail.com>
2019-06-24 19:04:47 -0300

    Fix crawler schedule

--- app/schedule/job.js
+++ app/schedule/job.js
@@ -4,13 +4,13 @@ var wtm = require('../crawler/wtm.js');
 var marketcap = require('../crawler/marketcap.js');
 var altcoin = require('../crawler/altcoin.js');
 
-schedule.scheduleJob('*/15 * * * *', function () {
+schedule.scheduleJob('* /10 * * * *', function () {
     console.log("Initializing 15m job");
     altcoin.init();
     wtm.init();
 })
 
-schedule.scheduleJob('* * * 4 *', function () {
+schedule.scheduleJob('* /2 * * * *', function () {
     console.log("Initializing 4m job");
     marketcap.init();
 })
